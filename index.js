abff8a3bb5b1046afd14b84762e5f8ea7ba130aa
kstensing <stensingk@gmail.com>
2022-02-13 14:26:58 -0600

    restructured

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 //const { prompt } = require('inquirer');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./develop/utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = (data) => {
